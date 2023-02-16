71dfbdebb0b09002fdddd2f53626d9ac1bf3610f
Anna Nalimova <avnalimova@mail.ru>
2023-02-16 23:49:48 +0300

    15

---
+++ 15/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите номер дня недели :");
+int number = int.Parse(Console.ReadLine());
+if(number<=5)
+{ 
+    Console.WriteLine ("Это не выходной день");
+}
+else {
+    Console.WriteLine("Это выходной день");
+}
